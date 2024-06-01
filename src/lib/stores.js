9ef243224c68d90f916cf4cd36bea2a5225ea167
dmihovch <dmihovch@gmail.com>
2024-06-01 18:21:21 -0400

    fixed stores

--- src/lib/stores.js
+++ src/lib/stores.js
@@ -1,5 +1,4 @@
 import { writable } from "svelte/store";
 
 export const appleSharePrice = writable(0);
-
-export const purchaseTrigger = writable(() => )
+export const purchaseTrigger = writable(0);
